76b8ebcbc15f891e146e52638d18584697c2804c
Gravity <tevoned@gmail.com>
2022-04-29 19:18:44 -0400

    updates to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,45 @@ const allCharacters = "";
 var createPass = function () {
   allCharacters = "";
   password = "";
+  passLength = 0;
+
+  while (passLength < 8 || passLength > 128) {
+    passLength = window.prompt(
+      "Please type a number between 8 and 128 for your password length"
+    );
+  }
+
+  const lowercaseConfirm = window.confirm("Would you like lowercase letters in your passwor? Click Ok if yes")
+  const uppercaseConfirm = window.confirm("Would you like uppercase letters in your password? Click OK if yes");
+  const numbersConfirm = window.confirm("Would you like numbers in your password? Click OK if yes");
+  const specialConfirm = window.confirm("Would you like special characters in your password? Click OK if yes");
+
+  if (lowercaseConfirm) {
+    allCharacters += lowercase;
+  };
+  if (uppercaseConfirm) {
+    allCharacters += uppercase;
+  };
+  if (numbersConfirm) {
+    allCharacters += numbers;
+  };
+  if (specialConfirm) {
+    allCharacters += specialCharacters;
+  };
+
+  console.log(allCharacters);
+
 }
 
+function generatePassword() {
+  createPass();
+  for (let i = 1; i <= passLength; i++) {
+    let randomNumber = Math.floor(Math.random() * allCharacters.length)
+    password += allCharacters[randomNumber];
+  }
+  return password;
+}
 
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
